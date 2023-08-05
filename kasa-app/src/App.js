81646ea0ac1c88831fe1aecd0a3c475550668f18
Paul Leblanc <azikx4qg@gmail.com>
2023-08-05 18:02:32 +0200

    deployy

--- kasa-app/src/App.js
+++ kasa-app/src/App.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 
 function App() {
    return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
          <Header />
          <Routes>
             <Route path="/about" element={<AboutPage />} />
